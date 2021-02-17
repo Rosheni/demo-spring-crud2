06a83765c050cf91f9110377822870f58126d6c1
Rosheni <38715798+Rosheni@users.noreply.github.com>
2021-02-17 20:08:15 +0530

    Update DemoSpringBootApplication.java

--- src/main/java/com/example/demospringboot/DemoSpringBootApplication.java
+++ src/main/java/com/example/demospringboot/DemoSpringBootApplication.java
@@ -1,31 +1,50 @@
 package com.example.demospringboot;
 
 import com.example.demospringboot.model.Customer;
+import com.example.demospringboot.model.Student;
 import com.example.demospringboot.repository.CustomerRepository;
+import com.example.demospringboot.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import javax.management.MXBean;
-
 @SpringBootApplication
 public class DemoSpringBootApplication {
 
     @Autowired
     private CustomerRepository custRepo;
-	public static void main(String[] args) {
+    private StudentRepository studRepo;
+
+    public DemoSpringBootApplication(StudentRepository studRepo) {
+        this.studRepo = studRepo;
+    }
+
+    public static void main(String[] args) {
 	    SpringApplication.run(DemoSpringBootApplication.class, args);
 	}
 
 	@Bean
     public CommandLineRunner insertRecords() {
 	    return (args) -> {
+
+//	customer records
 	        custRepo.save(new Customer("Rosheni" , "Kosanam"));
             custRepo.save(new Customer("Joe" , "Wills"));
             custRepo.save(new Customer("Robert" , "Jr"));
             custRepo.save(new Customer("Mia" , "Jones"));
+
+//  student records
+
+            studRepo.save(new Student ("Rosheni" , "Java" , "kosanamrosheni@gmail.com"));
+            studRepo.save(new Student ("Jasmin" , "Angular" ,"jasminflos@gmail.com"));
+            studRepo.save(new Student ("Bill" , "C","billgates@gmail.com"));
+            studRepo.save(new Student ("Riya" , "Python","riyamalhotra@gmail.com"));
+
         };
+
+
     }
 }
+
