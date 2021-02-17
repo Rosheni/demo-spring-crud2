9f517dd77df058312d641e801da9dc8a8a513018
Rosheni <38715798+Rosheni@users.noreply.github.com>
2021-02-17 20:09:24 +0530

    Add files via upload

---
+++ src/main/java/com/example/demospringboot/model/Student.java
@@ -0,0 +1,65 @@
+package com.example.demospringboot.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String subject;
+    private String email;
+
+    public Student(String name, String subject, String email){
+        this.name = name;
+        this.subject = subject;
+        this.email = email;
+    }
+
+    public Student() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+
